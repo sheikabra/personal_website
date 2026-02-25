ae1f402fc1844b0d1a70d7110eecf804a2551229
Abrahim Hussain <sheikabra@gmail.com>
2026-02-25 13:27:31 -0600

    Added TypeWriter Effect to Header

--- personal-website/src/App.jsx
+++ personal-website/src/App.jsx
@@ -1,7 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css' 
+import Typewriter from "typewriter-effect"
+
+
+
+
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,12 +21,24 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
-      <h1>Vite + React</h1>
+      </div>   
+        <h1>  
+          Hello 
+          <Typewriter 
+            component="span"
+            options={{ 
+              strings: [" World", " Dunya"],
+              autoStart: true,
+              loop: true,
+              cursor: '_',
+            }}
+          />
+      </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </button> 
+      
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
